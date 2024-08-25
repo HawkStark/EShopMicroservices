c8d8a7c8d01c32ced80642272463be2af8458188
Fabiano Santos <fabianobelarmino@hotmail.com>
2024-08-25 10:29:36 -0300

    Finished global exception handler.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,3 @@
-using BuildingBlocks.Behaviors;
-using FluentValidation;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,9 +17,40 @@ builder.Services.AddMarten(opts =>
 	opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+//app.UseExceptionHandler(exceptionHandlerApp =>
+//{
+//	exceptionHandlerApp.Run(async context =>
+//	{
+//		var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+//		if (exception == null)
+//		{
+//			return;
+//		}
+
+//		var problemDetails = new ProblemDetails
+//		{ 
+//			Title = exception.Message,
+//			Status = StatusCodes.Status500InternalServerError,
+//			Detail = exception.StackTrace
+//		};
+
+//		var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+//		logger.LogError(exception, exception.Message);
+
+//		context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+//		context.Response.ContentType = "application/problem+json";
+
+//		await context.Response.WriteAsJsonAsync(problemDetails);
+//	});
+//});
 
 app.Run();
